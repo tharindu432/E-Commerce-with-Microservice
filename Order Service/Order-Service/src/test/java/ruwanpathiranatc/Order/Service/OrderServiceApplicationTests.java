7e2c92eca8aa76a9d1c4fe93338506474fde28c4
tharindu432 <tharinduchaturanga432@gmail.com>
2025-03-07 11:45:00 +0530

    setting up the microservices

---
+++ Order Service/Order-Service/src/test/java/ruwanpathiranatc/Order/Service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ruwanpathiranatc.Order.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Stock Service/Stock-Service/src/test/java/ruwanpathiranatc/Stock/Service/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ruwanpathiranatc.Stock.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
